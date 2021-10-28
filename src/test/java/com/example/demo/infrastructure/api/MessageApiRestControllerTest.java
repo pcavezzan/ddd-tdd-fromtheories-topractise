757e748383ca67bb191a568e07b5bc037361ddc6
Patrice CAVEZZAN <pcavezzan@gmail.com>
2021-10-28 10:45:49 +0200

    Step 3 - Refactor
    Fix tests infrastructure part.

--- src/test/java/com/example/demo/infrastructure/api/MessageApiRestControllerTest.java
+++ src/test/java/com/example/demo/infrastructure/api/MessageApiRestControllerTest.java
@@ -1,15 +1,29 @@
 package com.example.demo.infrastructure.api;
 
+import com.example.demo.domain.Message;
+import com.example.demo.domain.MessagePort;
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.InjectMocks;
+import org.mockito.Mock;
+import org.mockito.junit.jupiter.MockitoExtension;
 
 import static org.assertj.core.api.Assertions.assertThat;
+import static org.mockito.Mockito.when;
 
+@ExtendWith(MockitoExtension.class)
 class MessageApiRestControllerTest {
 
-    private final MessageApiRestController controller = new MessageApiRestController();
+    @Mock
+    private MessagePort messagePort;
+
+    @InjectMocks
+    private MessageApiRestController controller;
 
     @Test
     void getWelcomeMessage() {
+        when(messagePort.getWelcomeMessage()).thenReturn(new Message("Welcome to TZebra"));
+
         final var welcomeMessage = controller.getWelcomeMessage();
 
         assertThat(welcomeMessage).isEqualTo(new MessageView("Welcome to TZebra"));
